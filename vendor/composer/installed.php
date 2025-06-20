2e5c918e4aee6ca48872fd1af29ef42950b4e526
Shane Barron <sbarron@daniellefence.net>
2025-06-20 14:52:54 -0400

    updates

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'daniellefence/buttons',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => null,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'daniellefence/buttons' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => null,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
